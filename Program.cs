7e4427fa792114301b81df4861ab08d1195aa644
Anna <azavas@yandex.ru>
2022-10-04 08:55:53 +0300

    Сделала 2 задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1>num2){
+    Console.WriteLine($"{num1} = max ");
+}
+else{
+    Console.WriteLine($"{num2} = max ");
+}
+*/
+
+/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите три числа: ");
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+if (num1>num2){
+        if (num1>num3) {
+            Console.WriteLine($"{num1} = max ");    
+        }
+        else{
+            Console.WriteLine($"{num3} = max ");
+        }
+}
+else{
+        if (num2>num3) {
+            Console.WriteLine($"{num2} = max ");
+        }
+        else{
+            Console.WriteLine($"{num3} = max ");
+        }
+}
+*/
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
